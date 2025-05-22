9f3a1f83b1f340fbd2e2aefeaf0a119daef74869
Ahmed-Ale <ahmed19aly@gmail.com>
2025-05-22 13:14:47 +0300

    add PostPlatform migration and model

---
+++ app/Models/PostPlatform.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class PostPlatform extends Pivot
+{
+    protected $table = 'post_platform';
+
+    protected $fillable = [
+        'post_id',
+        'platform_id',
+        'platform_status',
+    ];
+
+    public $timestamps = true;
+
+    protected $casts = [
+        'platform_status' => 'string',
+    ];
+}
