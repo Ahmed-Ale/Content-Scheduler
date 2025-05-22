74a4ebc59ae4b87d92b3bd16990ea3b39c547a05
Ahmed-Ale <ahmed19aly@gmail.com>
2025-05-22 13:08:17 +0300

    add factory and seeders for posts

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::first() ?? User::factory()->create();
+        Post::factory()->count(10)->create(['user_id' => $user->id]);
+    }
+}
