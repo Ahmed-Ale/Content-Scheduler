53a3bc87a637de65ead6684e1c7614f05c1f8489
Ahmed-Ale <ahmed19aly@gmail.com>
2025-05-25 21:10:42 +0300

    refactor logout method in AuthController to remove unused Request parameter

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
@@ -54,7 +53,7 @@ class AuthController extends Controller
         );
     }
 
-    public function logout(Request $request)
+    public function logout()
     {
         $user = Auth::user();
         $user->tokens()->delete();
