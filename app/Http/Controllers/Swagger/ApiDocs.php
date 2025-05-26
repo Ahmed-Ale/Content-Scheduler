443d970056ab64a9d5cc3911f03c200f03633f15
Ahmed-Ale <ahmed19aly@gmail.com>
2025-05-26 13:14:09 +0300

    add ApiDocs class for Swagger API documentation annotations

---
+++ app/Http/Controllers/Swagger/ApiDocs.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="Content Scheduler API",
+ *     version="1.0.0",
+ *     description="API documentation for the Content Scheduler application",
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Local development server"
+ * )
+ */
+class ApiDocs
+{
+    // This class is just a placeholder for Swagger annotations
+}
